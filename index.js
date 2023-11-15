5305f4b33b16827521701c76473c030f4d4e0ff0
jafar bino <binocoder@gmail.com>
2023-11-16 01:09:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(game => game.result === 'W');
+  
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
+  
